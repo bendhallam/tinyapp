b13055e9e3a3f305a0f9bd41b472e61ebe3fcd2c
Benjamin <bendhallam@gmail.com>
2024-07-18 17:29:51 -0600

    added urls page

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080; //default port 8080
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xL": "http://www.google.com",
@@ -21,11 +23,13 @@ app.get("/fetch", (req, res) => {
 });
 
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  const templateVars = { greeting: "Hello World!"};
+  res.render("hello_world", templateVars);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars)
 });
 
 app.listen(PORT, () => {
