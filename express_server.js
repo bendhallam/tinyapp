6652600bd52385ce8695547d7d37fee892537491
Benjamin <bendhallam@gmail.com>
2024-07-18 09:17:21 -0600

    updated express_server.js

--- express_server.js
+++ express_server.js
@@ -11,6 +11,15 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+});
+
+app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
+});
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
