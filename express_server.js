ec7ca750c8b50da4af86e72b04369ba0e7b15735
Benjamin <bendhallam@gmail.com>
2024-07-20 10:45:11 -0600

    added GET route and POST route

--- express_server.js
+++ express_server.js
@@ -2,6 +2,17 @@ const express = require("express");
 const app = express();
 const PORT = 8080; //default port 8080
 
+
+function generateRandomString() {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < 6; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(randomIndex);
+  }
+  return result;
+}
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -9,28 +20,31 @@ const urlDatabase = {
   "9sm5xL": "http://www.google.com",
 };
 
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send(generateRandomString());
+})
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
-
-app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
+  console.log(templateVars)
 });
 
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars)
+  res.render("urls_index", templateVars);
 });
 
 app.listen(PORT, () => {
